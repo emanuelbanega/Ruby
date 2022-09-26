5d74352c6add8637db942f7f4d0c80dd89a4070d
Luis Emanuel Banega <emabanega@hotmail.com>
2022-09-25 23:04:22 -0300

    Solucione el ejercicio 09

--- Practica 1/Ejercicio09.rb
+++ Practica 1/Ejercicio09.rb
@@ -10,8 +10,11 @@ def string_a_arreglo_ascii(cadena)
     return cadena.split("").map { | letra | letra.ord }
 end
 
+def string_remplaza_vocal(cadena)
+    return cadena.tr("aAeEiIoOuU", "4433110066")
+end
+
 puts string_reverso("Hola mundo") # => "odnum aloH"
 puts string_sin_espacio("Hola mundo") # => "Holamundo"
 puts string_a_arreglo_ascii("Hola mundo") # => [72, 111, 108, 97, 32, 109, 117, 110, 100, 111]
-
-#Falta el metodo de cambiar vocales
+puts string_remplaza_vocal("Hola mundo") # => H0l4 m6nd0
